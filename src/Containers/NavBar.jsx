cbe540039895297f7bedc23caf8f138ae1769061
RNtaate <royhyde14@gmail.com>
2024-03-29 20:54:42 +0300

    Updated the styling of logo
    Updated the styling in the app.jsx file

--- src/Containers/NavBar.jsx
+++ src/Containers/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav className="w-full flex py-4 px-5 justify-between items-center">
-      <h1 className="text-3xl font-bold font-robotoflex bg-gradient-to-b from-slate-800 to-slate-500 p-2 rounded-xl text-slate-300">
+      <h1 className="text-3xl font-bold font-robotoflex  p-2 rounded-xl text-slate-300">
         NORP.<span className="text-sm font-light">MOVIES</span>.
       </h1>
       <span className="text-3xl cursor-pointer text-slate-300">
