e103caff283d515e3667ecfa069b061ac205827d
RNtaate <royhyde14@gmail.com>
2024-03-29 14:51:02 +0300

    Update styling for the navbar and of main in app.jsx

--- src/Containers/NavBar.jsx
+++ src/Containers/NavBar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav className="w-full flex py-4 px-5 justify-between items-center">
-      <h1 className="text-3xl font-bold font-robotoflex">
-        NORP<span className="text-sm font-light">MOVIES</span>
+      <h1 className="text-3xl font-bold font-robotoflex bg-gradient-to-b from-slate-800 to-slate-500 p-2 rounded-xl text-slate-300">
+        NORP.<span className="text-sm font-light">MOVIES</span>.
       </h1>
-      <span className="text-3xl cursor-pointer">
+      <span className="text-3xl cursor-pointer text-slate-300">
         <i class="fa-brands fa-github"></i>
       </span>
     </nav>
