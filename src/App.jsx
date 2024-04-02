e0a99d9b05bfbc6a6389f5b180d6d9d36fca8a23
RNtaate <royhyde14@gmail.com>
2024-04-03 00:40:53 +0300

    Install keen-slider using npm
    add the css importation necessary for the proper workings of the keen-slider to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import 'keen-slider/keen-slider.min.css';
 
 import NavBar from './Containers/NavBar';
 import MoviesDataContextProvider from './Context/MoviesDataContextProvider';
