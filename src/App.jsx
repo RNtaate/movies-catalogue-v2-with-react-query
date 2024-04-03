a77f95626c4a3ce9b25a9cd96a3b4f5a9021d607
RNtaate <royhyde14@gmail.com>
2024-04-03 17:43:16 +0300

    Uninstall keen-slider using npm
    Remove all stylings and style files associated with keen-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import 'keen-slider/keen-slider.min.css';
 
 import NavBar from './Containers/NavBar';
 import MoviesDataContextProvider from './Context/MoviesDataContextProvider';
