f57c7316526a1539e8ef5d3fb60cada1c0f366d4
RNtaate <royhyde14@gmail.com>
2024-04-03 16:39:58 +0300

    Implement scrolling from left to right in the GenresContainer
    Comment out the code for later use soa as to facilitate easy implementation of the scrolling.
    
    Update the genres.json file to facilitate scroll testing.

--- src/Containers/GenresContainer.jsx
+++ src/Containers/GenresContainer.jsx
@@ -1,41 +1,112 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { MoviesDataContext } from '../Context/MoviesDataContextProvider';
 import { useQuery } from '@tanstack/react-query';
 
 import genresJsonData from '../utils/genres.json';
 import { wait } from '../Helpers/HelperMethods';
+import GenrePill from '../Components/GenrePill';
 
 const genres = genresJsonData.genres;
+const TRANSLATE_AMOUNT = 200;
 
 const GenresContainer = () => {
-  const moviesDataContext = useContext(MoviesDataContext);
-  const { genresList, setGenresList, nowPlayingMovies } = moviesDataContext;
-
-  const genresListQuery = useQuery({
-    queryKey: ['genres'],
-    enabled: nowPlayingMovies.length != 0,
-    queryFn: () => {
-      return wait(1000).then(() => [...genres]);
-    },
-  });
+  const [isLeftShowing, setIsLeftShowing] = useState(false);
+  const [isRightShowing, setIsRightShowing] = useState(false);
+  const [translate, setTranslate] = useState(0);
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    if (genresListQuery.isSuccess) {
-      setGenresList(genresListQuery.data);
-    }
-  }, [genresListQuery.isSuccess]);
+    if (containerRef.current == null) return;
 
-  if (genresListQuery.isLoading) return <div>Loading Genres...</div>;
-  if (genresListQuery.isError)
-    return <div>{JSON.stringify(genresListQuery.error)}</div>;
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0]?.target;
+      if (container == null) return;
+
+      setIsLeftShowing(translate > 0);
+      setIsRightShowing(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [genres, translate]);
+
+  // const moviesDataContext = useContext(MoviesDataContext);
+  // const { genresList, setGenresList, nowPlayingMovies } = moviesDataContext;
+
+  // const genresListQuery = useQuery({
+  //   queryKey: ['genres'],
+  //   enabled: nowPlayingMovies.length != 0,
+  //   queryFn: () => {
+  //     return wait(1000).then(() => [...genres]);
+  //   },
+  // });
+
+  // useEffect(() => {
+  //   if (genresListQuery.isSuccess) {
+  //     setGenresList(genresListQuery.data);
+  //   }
+  // }, [genresListQuery.isSuccess]);
+
+  // if (genresListQuery.isLoading) return <div>Loading Genres...</div>;
+  // if (genresListQuery.isError)
+  //   return <div>{JSON.stringify(genresListQuery.error)}</div>;
 
   return (
-    <div>
-      {genresListQuery.isSuccess && <h3>GENRES</h3>}
-      {genresList.map((genre) => (
-        <span key={genre.id}>{genre.name}</span>
-      ))}
-    </div>
+    <section className="px-5">
+      <div ref={containerRef} className="max-w-screen overflow-hidden relative">
+        <div
+          className="transition-transform whitespace-nowrap flex gap-2 w-[max-content]"
+          style={{ transform: `translateX(-${translate}px)` }}
+        >
+          {genres.map((genre) => {
+            return <GenrePill genre={genre} />;
+          })}
+        </div>
+        {isLeftShowing && (
+          <div className="left-angle px-2 absolute h-full aspect-square w-16 md:w-32 top-0 flex items-center bg-gradient-to-r from-slate-900">
+            <button
+              type="button"
+              className="flex justify-center items-center w-8 aspect-square rounded-full hover:ring-slate-500 hover:ring-1"
+              onClick={() => {
+                setTranslate((prevTranslate) => {
+                  const newTranslate = prevTranslate - TRANSLATE_AMOUNT;
+                  if (newTranslate < 0) return 0;
+                  return newTranslate;
+                });
+              }}
+            >
+              <i className="fa-solid fa-angle-left"></i>
+            </button>
+          </div>
+        )}
+
+        {isRightShowing && (
+          <div className="left-angle px-2 absolute h-full aspect-square w-16 md:w-32 top-0 right-0 flex items-center justify-end bg-gradient-to-l from-slate-900">
+            <button
+              type="button"
+              className="flex justify-center items-center w-8 aspect-square rounded-full hover:ring-slate-500 hover:ring-1"
+              onClick={() => {
+                setTranslate((prevTranslate) => {
+                  const newTranslate = prevTranslate + TRANSLATE_AMOUNT;
+                  const edge = containerRef.current.scrollWidth;
+                  const width = containerRef.current.clientWidth;
+                  console.log(edge, width);
+                  if (newTranslate + width >= edge) return edge - width;
+                  return newTranslate;
+                });
+              }}
+            >
+              <i className="fa-solid fa-angle-right"></i>
+            </button>
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
