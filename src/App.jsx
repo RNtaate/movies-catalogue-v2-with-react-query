8b617895117129228d78f93e8737f78f5f49c330
RNtaate <royhyde14@gmail.com>
2024-04-05 18:28:12 +0300

    Transfer the navbar to the Home page component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import 'react-activity/dist/library.css';
 
-import NavBar from './Containers/NavBar';
 import MoviesDataContextProvider from './Context/MoviesDataContextProvider';
 import Home from './pages/Home';
 import MovieDetails from './pages/MovieDetails';
@@ -12,7 +11,6 @@ function App() {
   return (
     <MoviesDataContextProvider>
       <main className="w-screen min-h-screen max-h-screen overflow-auto backdrop-blur-lg bg-white/10 text-white font-montserrat">
-        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import NowPlayingSectionContainer from '../Containers/NowPlayingSectionContainer';
 import GenresContainer from '../Containers/GenresContainer';
 import MoviesMainSection from '../Containers/MoviesMainSection';
+import NavBar from '../Containers/NavBar';
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       <NowPlayingSectionContainer />
       <GenresContainer />
       <MoviesMainSection />
