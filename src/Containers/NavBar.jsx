2b4c572ec270eece90c0ef97b4d06f43bc0c50e6
RNtaate <royhyde14@gmail.com>
2024-04-08 21:30:27 +0300

    Import and use the SearchWithGenres component in the HomePage.
    Implement the opening of the SearchWithGenres component in the navbar, add another icon in the navbar.

--- src/Containers/NavBar.jsx
+++ src/Containers/NavBar.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { MoviesDataContext } from '../Context/MoviesDataContextProvider';
 
 const NavBar = () => {
+  const movieDataContext = useContext(MoviesDataContext);
+  const { isSearchWithGenresOpen, setIsSearchWithGenresOpen } =
+    movieDataContext;
+
   return (
     <nav className="w-full flex flex-col md:flex-row gap-4 py-4 px-5 justify-between md:items-center">
       <h1 className="text-3xl font-bold font-robotoflex  p-2 rounded-xl text-slate-300">
         NORP.<span className="text-sm font-light">MOVIES</span>.
       </h1>
-      <span className="inline-block font-robotoflex text-lg cursor-pointer text-slate-300 flex items-center justify-between flex-shrink-0 px-8 py-2 bg-slate-900/[.6] rounded-full hover:ring-1 hover:ring-slate-500 transition-all">
+      <span
+        className="inline-block font-robotoflex text-lg cursor-pointer text-slate-300 flex items-center justify-between flex-shrink-0 px-8 py-2 bg-slate-900/[.6] rounded-full hover:ring-1 hover:ring-slate-500 transition-all"
+        onClick={() => setIsSearchWithGenresOpen(true)}
+      >
         Search
         <i className="fa-solid fa-magnifying-glass ml-4"></i>
       </span>
-      <span className="text-3xl cursor-pointer text-slate-300 hover:text-slate-500 transition-all">
-        <i className="fa-brands fa-github"></i>
+      <span className="flex gap-3 text-3xl cursor-pointer text-slate-300">
+        <i className="fa-brands fa-github hover:text-slate-500 transition-all"></i>
+        <i className="fa-brands fa-linkedin hover:text-slate-500 transition-all"></i>
       </span>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NowPlayingSectionContainer from '../Containers/NowPlayingSectionContainer';
 import GenresContainer from '../Containers/GenresContainer';
 import MoviesMainSection from '../Containers/MoviesMainSection';
 import NavBar from '../Containers/NavBar';
+import SearchWithGenres from '../Components/SearchWithGenres';
+import { MoviesDataContext } from '../Context/MoviesDataContextProvider';
 
 const Home = () => {
+  const movieDataContext = useContext(MoviesDataContext);
+  const { isSearchWithGenresOpen, setIsSearchWithGenresOpen } =
+    movieDataContext;
+
   return (
-    <>
-      <NavBar />
-      <NowPlayingSectionContainer />
-      <GenresContainer />
-      <MoviesMainSection />
-    </>
+    <section className="min-w-screen max-w-screen min-h-screen max-h-screen overflow-hidden relative">
+      <div className="overflow-auto max-h-screen">
+        <NavBar />
+        <NowPlayingSectionContainer />
+        <GenresContainer />
+        <MoviesMainSection />
+      </div>
+      {isSearchWithGenresOpen && <SearchWithGenres />}
+    </section>
   );
 };
 
