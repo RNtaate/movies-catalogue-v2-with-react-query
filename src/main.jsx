76e56a9daab182709feb3652e9d16b01c246ad41
RNtaate <royhyde14@gmail.com>
2024-04-09 18:54:04 +0300

    Add default options to the query client for stale time in react query.

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,13 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 30,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
