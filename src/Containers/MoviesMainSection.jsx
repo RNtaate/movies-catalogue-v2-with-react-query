ccad875c7c008969c380aef076c36aa1b69f73b1
RNtaate <royhyde14@gmail.com>
2024-04-09 18:57:46 +0300

    Set up the first draft for the infinte fetching in the MoviesMainSection component.
    Use mock data for genres to select genres for fetching.

--- src/Containers/MoviesMainSection.jsx
+++ src/Containers/MoviesMainSection.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { MoviesDataContext } from '../Context/MoviesDataContextProvider';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 
 import { wait } from '../Helpers/HelperMethods';
+import { getMoviesList } from '../Helpers/HelperFetchMethods';
 import moviesData from '../utils/movies.json';
+import genres from '../utils/genres.json';
 import { NavLink } from 'react-router-dom';
 import MovieBrick from '../Components/MovieBrick';
 import SpinnerLoader from '../Components/loaders/SpinnerLoader';
@@ -11,43 +13,76 @@ import LoadingError from '../Components/ErrorComponents/LoadingError';
 import YearsSelect from '../Components/YearsSelect';
 
 const moviesList = moviesData.results;
+const localgenres = genres.genres;
+const MAXIMUM_PAGES = 5;
 
 const MoviesMainSection = () => {
   const moviesDataContext = useContext(MoviesDataContext);
   const { moviesMainList, setMoviesMainList, genresList, selectedGenre } =
     moviesDataContext;
 
-  const movieListQuery = useQuery({
+  const {
+    status,
+    error,
+    data,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
     queryKey: ['movies'],
     enabled: genresList.length != 0,
-    queryFn: () => {
-      return wait(4000).then(() => moviesList);
+    getNextPageParam: (prevData) => {
+      console.log('This is previous data', prevData);
+      const nextPageNumber = prevData.data.page + 1;
+      const totalPages = prevData.data.total_pages;
+      if (nextPageNumber <= MAXIMUM_PAGES && nextPageNumber <= totalPages) {
+        return nextPageNumber;
+      }
+    },
+    queryFn: ({ pageParam = 1 }) => {
+      return getMoviesList(2024, localgenres[0].id, pageParam).then(
+        (res) => res
+      );
     },
   });
 
   useEffect(() => {
-    if (movieListQuery.isSuccess) {
-      setMoviesMainList(movieListQuery.data);
+    if (status == 'success') {
+      console.log(data);
+      setMoviesMainList(data);
     }
-  }, [movieListQuery.isSuccess]);
+  }, [status, data]);
 
-  if (movieListQuery.isLoading) return <SpinnerLoader variant="sentry" />;
-  if (movieListQuery.isError)
-    return <LoadingError message={JSON.stringify(movieListQuery.error)} />;
+  if (status == 'loading') return <SpinnerLoader variant="sentry" />;
+  if (status == 'error')
+    return <LoadingError message={JSON.stringify(error)} />;
 
   return (
     <section className="flex flex-col px-5">
-      {movieListQuery.isSuccess && (
+      {/* {movieListQuery.isSuccess && (
         <div className="py-5 mt-4 font-robotoflex text-md font-bold flex items-center justify-between">
           <h2 className="">{selectedGenre.name}</h2>
           <YearsSelect />
         </div>
-      )}
+      )} */}
       <section className="flex flex-wrap justify-evenly gap-2 pb-8">
-        {moviesMainList.map((movie) => {
-          return <MovieBrick key={movie.id} movie={movie} />;
-        })}
+        {moviesMainList != null &&
+          moviesMainList.pages
+            .flatMap((moviesData) => moviesData.data.results)
+            .map((movie) => {
+              return <MovieBrick key={movie.id} movie={movie} />;
+            })}
       </section>
+      {hasNextPage && (
+        <div className="bg-red-500">
+          <button
+            type="button py-2 px-5 border border-slate-500 bg-zinc-500"
+            onClick={() => fetchNextPage()}
+          >
+            {isFetchingNextPage ? 'Loading...' : 'Load More'}
+          </button>
+        </div>
+      )}
     </section>
   );
 };
