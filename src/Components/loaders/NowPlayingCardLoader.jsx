a979e17658da3a8675b2e7bc2f4fc5a0a80ef431
RNtaate <royhyde14@gmail.com>
2024-04-11 14:15:37 +0300

    Update the design for NowPlayingCardLoader component to included rounded corners or not

--- src/Components/loaders/NowPlayingCardLoader.jsx
+++ src/Components/loaders/NowPlayingCardLoader.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import loaderImage from '../../assets/loader-image-1.svg';
 
-const NowPlayingCardLoader = () => {
+const NowPlayingCardLoader = ({ rounded }) => {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-500 rounded-2xl overflow-hidden transition-all z-2">
+    <div
+      className={`absolute top-0 left-0 right-0 bottom-0 bg-slate-500 ${
+        rounded ? 'rounded-2xl' : 'rounded-none'
+      } overflow-hidden transition-all z-2`}
+    >
       <img
         alt="loading image"
         src={loaderImage}
