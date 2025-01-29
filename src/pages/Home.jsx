e928c24408fe027b2bda20a47e363a022babdea3
RNtaate <royhyde14@gmail.com>
2025-01-29 12:24:18 +0300

    Add scrolPosition tracking variables to the context for the homepage
    Add useEffect hook to implement scrollPosition tracking in the homepage component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
+
 import NowPlayingSectionContainer from '../Containers/NowPlayingSectionContainer';
 import GenresContainer from '../Containers/GenresContainer';
 import MoviesMainSection from '../Containers/MoviesMainSection';
 import NavBar from '../Containers/NavBar';
 import SearchWithGenres from '../Components/SearchWithGenres';
 import { MoviesDataContext } from '../Context/MoviesDataContextProvider';
+import useScrollToPosition from '../hooks/useScrollToPosition';
 
 const Home = () => {
   const movieDataContext = useContext(MoviesDataContext);
-  const { isSearchWithGenresOpen, setIsSearchWithGenresOpen } =
-    movieDataContext;
+  const {
+    isSearchWithGenresOpen,
+    setIsSearchWithGenresOpen,
+    homePageScrollPos,
+    setHomePageScrollPos,
+  } = movieDataContext;
+
+  const scrollableDivRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollableDivRef.current) {
+      const handleScrollPos = (scrollTop) => setHomePageScrollPos(scrollTop);
+
+      scrollableDivRef.current.addEventListener('scroll', (e) => {
+        handleScrollPos(e.target.scrollTop);
+      });
+
+      return scrollableDivRef.current.removeEventListener('scroll', (e) => {
+        handleScrollPos(e.target.scrollTop);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (scrollableDivRef.current) {
+      scrollableDivRef.current.scrollTop = homePageScrollPos;
+    }
+  }, []);
 
   return (
     <section className="min-w-screen max-w-screen min-h-screen max-h-screen overflow-hidden relative select-none">
-      <div className="overflow-auto max-h-screen">
+      <div className="overflow-auto max-h-screen" ref={scrollableDivRef}>
         <NavBar />
         <NowPlayingSectionContainer />
         <GenresContainer />
